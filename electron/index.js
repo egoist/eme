db0ec072ea7ad0fef137810e3f1fdc96c14d7b5a
vai <ncysatnaf@gmail.com>
2016-07-20 16:13:05 +0800

    OS X can't quit

--- electron/index.js
+++ electron/index.js
@@ -32,7 +32,7 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+  if (process.platform == 'darwin') {
     app.quit()
   }
 })
