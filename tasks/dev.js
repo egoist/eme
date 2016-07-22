89bceb57df0cb60ee11a388ec27675e7ed340ee0
EGOIST <0x142857@gmail.com>
2016-07-22 11:32:06 +0800

    use scripy

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,41 +1,16 @@
 #!/usr/bin/env node
 'use strict'
-const child = require('child_process')
+const scripy = require('scripy')
 
 process.env.NODE_ENV = 'development'
 const port = 8020
 let bundled = false
 
-const webpack = child.spawn('npm', ['run', 'dev-server'])
+const webpack = scripy('npm run dev-server', {displayName: 'webpack'})
 
 webpack.stdout.on('data', (data) => {
-  console.log(`[webpack] stdout: ${data}`)
   if (!bundled && data.indexOf('webpack: bundle is now VALID') !== -1) {
     bundled = true
-    runElectron()
+    scripy('npm run app', {displayName: 'electron'})
   }
 })
-
-webpack.stderr.on('data', (data) => {
-  console.log(`[webpack] stderr: ${data}`)
-})
-
-webpack.on('close', (code) => {
-  console.log(`[webpack] child process exited with code ${code}`)
-})
-
-function runElectron() {
-  const electron = child.spawn('npm', ['run', 'app'])
-
-  electron.stdout.on('data', (data) => {
-    console.log(`[electron] stdout: ${data}`)
-  })
-
-  electron.stderr.on('data', (data) => {
-    console.log(`[electron] stderr: ${data}`)
-  })
-
-  electron.on('close', (code) => {
-    console.log(`[electron] child process exited with code ${code}`)
-  })
-}
