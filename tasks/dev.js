00f2e921fef6ea028d001ecf6694cb316fbfa9c9
EGOIST <0x142857@gmail.com>
2016-08-20 22:31:06 +0800

    fix script name

--- tasks/dev.js
+++ tasks/dev.js
@@ -7,9 +7,9 @@ let bundled = false
 
 const webpack = scripy('npm run watch', {displayName: 'webpack'})
 
-webpack.stdout.on('data', (data) => {
+webpack.stdout.on('data', data => {
   if (!bundled && data.indexOf('bundle.js') !== -1) {
     bundled = true
-    scripy('npm run app', {displayName: 'electron'})
+    scripy('npm start', {displayName: 'electron'})
   }
 })
