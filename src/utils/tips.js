9d1b9d08ab9c48518ae76a1c7bcbd165a038a090
EGOIST <0x142857@gmail.com>
2016-08-23 00:00:19 +0800

    add tips

--- src/utils/tips.js
+++ src/utils/tips.js
@@ -4,5 +4,8 @@ const metaKey = isMac ? 'Command' : 'Ctrl'
 
 export default [
   `Use <kbd>${metaKey} \\</kbd> to toggle focus mode`,
-  `You can drag and drop a file in the editor`
+  `You can drag and drop a file in the editor`,
+  `Double click the header to create new tab`,
+  `Use <kbd>${metaKey} Shift \\</kbd> to toggle writing mode`,
+  `Try distraction free mode <kbd>${metaKey} J</kbd> when you enter full screen`
 ]
