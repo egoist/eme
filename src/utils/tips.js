bc98c1cc4ecf77d928e1ca0275ce72ff773c6a4e
EGOIST <0x142857@gmail.com>
2016-09-24 19:57:50 +0800

    update tips

--- src/utils/tips.js
+++ src/utils/tips.js
@@ -6,6 +6,7 @@ export default [
   `Use <kbd>${metaKey} \\</kbd> to toggle focus mode`,
   `You can drag and drop a file in the editor`,
   `Double click the header to create new tab`,
-  `Use <kbd>${metaKey} Shift \\</kbd> to toggle writing mode`,
-  `Try distraction free mode <kbd>${metaKey} J</kbd> when you enter full screen`
+  `Use <kbd>${metaKey} Shift M</kbd> to switch writing mode`,
+  `Try distraction free mode <kbd>${metaKey} J</kbd> when you enter full screen`,
+  `Arrow keys are available in \`Preview Only\` mode to navigate between slides`
 ]
